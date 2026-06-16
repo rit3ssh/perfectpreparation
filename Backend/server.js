bbaa47ec804379d43a12854a23986e77e9625be5
Ritesh078bct <078bct068@ioepc.edu.np>
2026-06-16 16:41:16 +0545

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,22 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(
+  path.join(__dirname, "../Frontend/dist")
+));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../Frontend/dist/index.html")
+  );
+});
+
 connectToDB()
 
 const PORT = process.env.PORT || 3000
