6fb9c5db4a656f8f0e4962a5f1dc6f10b05f5d88
Ritesh078bct <078bct068@ioepc.edu.np>
2026-06-16 16:33:40 +0545

    ..

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL,
+    baseURL: "http://localhost:3000",
     withCredentials: true,
 })
 
